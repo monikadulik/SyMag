62cd4c581de3de950f519f6687a51d94d6f569f1
Monika Dulik <monikaadulik@gmail.com>
2018-11-13 22:01:05 +0100

    warehouse filtering inputs added

--- resources/views/warehouse.blade.php
+++ resources/views/warehouse.blade.php
@@ -11,8 +11,44 @@
 </head>
 <body>
 @include('partials.header')
-<p>- Warehouse View  -</p>
 
+
+{{--TODO -> Wczytywanie listy magazynów z bazy--}}
+{{--TODO -> Wyszukiwanie według podanych argumentów--}}
+<div class="container">
+
+<div class="row mb-3 mt-3">
+    <button class="btn btn-outline-dark mt-4 mr-5 h-50" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+        Filtruj wyniki
+    </button>
+    <div class="collapse" id="collapseExample">
+        <div class="row">
+            <div class="col-md-4 mb-3">
+                <label for="warehouse">Magazyn</label>
+                <select class="custom-select d-block w-100" id="warehouse">
+                    <option value="">Wybierz z listy...</option>
+                    <option>Magazyn 1</option>
+                    <option>Magazyn 2</option>
+                    <option>Magazyn 3</option>
+                </select>
+            </div>
+            <div class="col-md-4 mb-3">
+                <label for="catalog-num">Numer katalogowy</label>
+                <input type="text" class="form-control" id="catalog-num" placeholder="Wpisz numer..." value="">
+            </div>
+            <div class="col-md-4 mb-3">
+                <label for="catalog-num">Szukaj według nazwy towaru</label>
+                <input type="text" class="form-control" id="catalog-num" placeholder="Wpisz nazwe..." value="">
+            </div>
+            <button class="btn btn-primary col-md-3 ml-3 h-50" type="submit">Szukaj</button>
+        </div>
+    </div>
+</div>
+
+    <p>- Warehouse View  -</p>
+
+</div>
+</div>
 
 
 
